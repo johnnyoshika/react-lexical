731b155d308501fc3c010cd6119ed2c1248eae48
Johnny Oshika <johnnyoshika@gmail.com>
2023-09-04 18:51:30 -0700

    Wire up left, center, right, undo, redo

--- src/Editor/Toolbar/useOnClickListener.ts
+++ src/Editor/Toolbar/useOnClickListener.ts
@@ -1,6 +1,11 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { eventTypes } from './toolbarIconsList';
-import { FORMAT_TEXT_COMMAND } from 'lexical';
+import {
+  FORMAT_ELEMENT_COMMAND,
+  FORMAT_TEXT_COMMAND,
+  REDO_COMMAND,
+  UNDO_COMMAND,
+} from 'lexical';
 
 const useOnClickListener = () => {
   const [editor] = useLexicalComposerContext();
@@ -14,6 +19,21 @@ const useOnClickListener = () => {
 
     if (e === eventTypes.formatUnderline)
       editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline');
+
+    if (e === eventTypes.formatAlignLeft)
+      editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'left');
+
+    if (e === eventTypes.formatAlignCenter)
+      editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'center');
+
+    if (e === eventTypes.formatAlignRight)
+      editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'right');
+
+    if (e === eventTypes.formatUndo)
+      editor.dispatchCommand(UNDO_COMMAND, undefined);
+
+    if (e === eventTypes.formatRedo)
+      editor.dispatchCommand(REDO_COMMAND, undefined);
   };
 
   return { onClick };
