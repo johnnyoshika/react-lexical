9a78c5a2583e1a3ec80076795f3fd71e2878b78f
Johnny Oshika <johnnyoshika@gmail.com>
2023-09-04 18:15:48 -0700

    Wire up bold and italic

--- src/Editor/Toolbar/useOnClickListener.ts
+++ src/Editor/Toolbar/useOnClickListener.ts
@@ -1,6 +1,16 @@
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { eventTypes } from './toolbarIconsList';
+import { FORMAT_TEXT_COMMAND } from 'lexical';
+
 const useOnClickListener = () => {
+  const [editor] = useLexicalComposerContext();
+
   const onClick = (e: string) => {
-    console.log('event is', e);
+    if (e === eventTypes.formatBold)
+      editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold');
+
+    if (e === eventTypes.formatItalic)
+      editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic');
   };
 
   return { onClick };
