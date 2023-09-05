fd5ffdb1f90468520f070253c58dd00339550895
Johnny Oshika <johnnyoshika@gmail.com>
2023-09-05 07:28:39 -0700

    Remove unused icons

--- src/Editor/Toolbar/index.tsx
+++ src/Editor/Toolbar/index.tsx
@@ -1,18 +1,14 @@
 import { Grid } from '@mui/material';
-import pluginsList from './toolbarIconsList';
+import iconsList from './toolbarIconsList';
 import useOnClickListener from './useOnClickListener';
 
 const Toolbar = () => {
   const { onClick } = useOnClickListener();
 
   return (
-    <Grid
-      container
-      sx={{ background: 'white', p: 1 }}
-      justifyContent="space-between"
-    >
-      {pluginsList.map(plugin => (
-        <Grid item key={plugin.id}>
+    <Grid container sx={{ background: 'white', p: 1 }}>
+      {iconsList.map(plugin => (
+        <Grid item key={plugin.id} mr={2}>
           <plugin.Icon onClick={() => onClick(plugin.event)} />
         </Grid>
       ))}
